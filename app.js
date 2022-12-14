fe6784b06b28b9a70822339cf513ebe4ad0aba63
Patrick Nepp <ptnepp@gmail.com>
2022-12-13 19:02:03 -0600

    project data

---
+++ app.js
@@ -0,0 +1,100 @@
+const data = [
+    {
+        id: 1,
+        img: "imgs/IMG_3210.JPG",
+        location: 'Current River - Family Float',
+        date: 'October',
+        cats: "Current River"
+    },
+    {
+        id: 2,
+        img: "imgs/IMG_2196.JPG",
+        location: 'Current River - Family Float',
+        date: 'October',
+        cats: "Current River"
+    },
+    {
+        id: 3,
+        img: "imgs/IMG_3254.JPG",
+        location: 'Current River - Cave',
+        date: 'October',
+        cats: "Current River"
+    },
+    {
+        id: 4,
+        img: "imgs/IMG_3258.JPG",
+        location: 'Current River - Cave',
+        date: 'October',
+        cats: "Current River"
+    },
+    {
+        id: 5,
+        img: "imgs/IMG_3222.jpg",
+        location: 'Current River - Kayaker',
+        date: 'October',
+        cats: "Current River"
+    },
+    {
+        id: 6,
+        img: "imgs/IMG_3244.JPG",
+        location: 'Current River',
+        date: 'October',
+        cats: "Current River"
+    },
+    {
+        id: 7,
+        img: "imgs/IMG_3121.JPG",
+        location: 'Mammoth Cave',
+        date: 'October',
+        cats: "Mammoth Cave"
+    },
+    {
+        id: 8,
+        img: "imgs/IMG_3131.JPG",
+        location: 'Mammoth Cave',
+        date: 'October',
+        cats: "Mammoth Cave"
+    },
+    {
+        id: 9,
+        img: "imgs/IMG_3134.JPG",
+        location: 'Mammoth Cave',
+        date: 'October',
+        cats: "Mammoth Cave"
+    },
+    {
+        id: 10,
+        img: "imgs/IMG_3123.JPG",
+        location: 'Mammoth Cave',
+        date: 'October',
+        cats: "Mammoth Cave"
+    },
+    {
+        id: 11,
+        img: "imgs/IMG_3119.JPG",
+        location: 'Mammoth Cave',
+        date: 'October',
+        cats: "Mammoth Cave"
+    },
+    {
+        id: 12,
+        img: "imgs/IMG_2861.JPG",
+        location: 'Eleven Point River - Frog',
+        date: 'September',
+        cats: "Eleven Point River"
+    },
+    {
+        id: 13,
+        img: "imgs/IMG_2868.JPG",
+        location: 'Eleven Point River - Kayak',
+        date: 'September',
+        cats: "Eleven Point River"
+    },
+    {
+        id: 14,
+        img: "imgs/IMG_2951.JPG",
+        location: 'Eleven Point River',
+        date: 'September',
+        cats: "Eleven Point River"
+    },
+]
